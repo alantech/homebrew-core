126f94c83dafdd52c23c1c50619b03cc390e7155
Alejandro Guillen <aguillenv93@gmail.com>
2021-03-12 11:23:25 +0100

    Update alan to v0.1.34

--- Formula/alan.rb
+++ Formula/alan.rb
@@ -1,8 +1,8 @@
 class Alan < Formula
   desc "Autoscalable Programming Language"
   homepage "https://github.com/alantech/alan"
-  url "https://github.com/alantech/alan/releases/download/v0.1.32-1/alan-macos.tar.gz"
-  sha256 "8982d51b67fbddddf7686c6d2dbb91d950b51d80ac58341ed3c6b007219a54a9"
+  url "https://github.com/alantech/alan/releases/download/v0.1.34/alan-macos.tar.gz"
+  sha256 "1114cf7ceb5e9d2db92f2a3a38f9e05f33d9efdbb677b367a8e2405530b271e8"
   license "Apache-2.0"
   revision 1
 
