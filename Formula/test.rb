77046bc4e0023b001b9d62fb465cb9a5a37c88f9
aguillenv <aguillenv@users.noreply.github.com>
2021-05-26 11:24:01 +0000

    Bump version v0.1.36

--- Formula/test.rb
+++ Formula/test.rb
@@ -1,7 +1,7 @@
 class Anycloud < Formula
   desc "Elastically scale webservers in any cloud provider"
   homepage "https://github.com/alantech/anycloud"
-  url "https://github.com/alantech/alan/releases/download/v0.1.41/anycloud-macos.tar.gz"
+  url "https://github.com/alantech/alan/releases/download/v0.1.36/anycloud-macos.tar.gz"
   sha256 "f352978f13eb0ee7e81cc59bd08da30ef5be5b6584259cf908eae3c329f6eb4f"
   license "Apache-2.0"
 
